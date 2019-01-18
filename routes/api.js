e79399a745bbed40ba374b66b53e13a66e500ec8
Bill McCutchan <bmccutchanjr@gmail.com>
2019-01-18 16:49:41 -0500

    removed socket.io

--- routes/api.js
+++ routes/api.js
@@ -7,58 +7,50 @@ const scrape = require ("./scrape");
 
 const database = require ("../data/database");
 
-const apiRoutes = (io) =>
-{   // This is a little different than how I usually set up a routing module, but this module needs
-    // a reference to 'io' which is in server.js.  To pass that reference, I need server.js to call a
-    // function, which in turn will return a reference to router -- which is what server.js needs.
+const app = express ();
+const router = express.Router ();
+app.use ("/", router);
 
-    const app = express ();
-    const router = express.Router ();
-    app.use ("/", router);
+router
+.use (function (request, result, next)
+{   // This always happens, no matter what route is requested.
 
-    router
-    .use (function (request, result, next)
-    {   // This always happens, no matter what route is requested.
+    console.log(chalk.magenta("api.js: ", request.url))
+    next();
+})
+.get ("/scrape", function (_, response)
+{   // This handles the API route /api/scrape
 
-        console.log(chalk.magenta("api.js: ", request.url))
-        next();
-    })
-    .get ("/scrape", function (_, response)
-    {   // This handles the API route /api/scrape
-
-        scrape ()
-        .then (function (result)
-        {
-            response.status(200).json (result);
-        })
-        .catch (function (error)
-        {   response.status(500).send(error);
-        })
-    })
-    .post ("/save", function (request, response)
-    {   // This is called when someone want to save an article.  The data to be saved is passed in the
-        // request header.  This scraper does not automatically save scraped articles (but only on command
-        // from the user).
-
-        const { headline, href } = request.body;
-        database.saveArticle (headline, href)
-        .then (function (result)
-        {   // I'm thinking this needs to return the updated list of saved articles...
-
-            console.log (chalk.magenta(result));
-            response.status(200).json(result);
-        })
-        .catch (function(error)
-        {
-            response.status(500).send(error)
-        })
-    })
-    .post ("/delete/:id", function (request, response)
+    scrape ()
+    .then (function (result)
     {
-
+        response.status(200).json (result);
     })
-    
-    return router;
-}
+    .catch (function (error)
+    {   response.status(500).send(error);
+    })
+})
+.post ("/save", function (request, response)
+{   // This is called when someone want to save an article.  The data to be saved is passed in the
+    // request header.  This scraper does not automatically save scraped articles (but only on command
+    // from the user).
 
-module.exports = apiRoutes;
+    const { headline, href } = request.body;
+    database.saveArticle (headline, href)
+    .then (function (result)
+    {   // I'm thinking this needs to return the updated list of saved articles...
+
+        console.log (chalk.magenta(result));
+        response.status(200).json(result);
+    })
+    .catch (function(error)
+    {
+        response.status(500).send(error)
+    })
+})
+.post ("/delete/:id", function (request, response)
+{
+
+})
+    
+module.exports = router;
--- server.js
+++ server.js
@@ -4,15 +4,9 @@
 // Require the Node modules needed for the application
 const chalk = require("chalk");
 const express = require("express");
-// 01   This is the socket.io stuff...seems to be preventing my express server from working at the
-//      moment...
-// 01 const express = require("express")();
-// 01 const server = require("http").Server(express);
-// 01 const io = require("socket.io")(server);
 
 // And get my modules
-// 01 const api = require("./routes/api")(io);
-const api = require("./routes/api")();
+const api = require("./routes/api");
 // const api = require("./routes/html");
 
 // Configure Express.js
@@ -26,11 +20,6 @@ app.use('/api', api);
 
 const PORT = process.env.PORT || 3030;
 
-// 01 server.listen(PORT, function()
 app.listen(PORT, function()
 {   console.log(chalk.green("server listening on port: ", PORT))
 })
-
-// 01 io.on ("connection", function(socket)
-// 01 {   socket.emit("Hello");
-// 01 })
